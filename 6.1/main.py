af0ccacb1934f0701516b0b4d9960ff010713a3c
NeNoBiL <maxtem1109@gmail.com>
2026-05-18 11:12:04 +0300

    Add files via upload

--- 6.1/main.py
+++ 6.1/main.py
@@ -1,1 +1,27 @@
+from fastapi import FastAPI, Depends, status, HTTPException
+from fastapi.security import HTTPBasic, HTTPBasicCredentials, OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from models import UserBase, UserInDB, User
 
+# 6.1
+app = FastAPI()
+security = HTTPBasic()
+
+users_db = {
+    "David": User(username="David", password="12345"),
+}
+
+
+def authenticate_user(creds: HTTPBasicCredentials = Depends(security)):
+    user = users_db.get(creds.username)
+    if not user or user.password != creds.password:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Basic"}
+        )
+    return user
+
+
+@app.get("/login", tags=["Login"], summary="Login user")
+async def login(user: User = Depends(authenticate_user)):
+    return {"message": f"Welcome, {user.username}!"}
